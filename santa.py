31c3c6ff72ca7a4b13b6391390db93dce26ef4b9
Jessica Sattler <jessicasattler@jessicas-air.lan>
2021-11-27 15:32:22 -0600

    People are assigned gifts but no try catch

--- santa.py
+++ santa.py
@@ -8,11 +8,15 @@ people_df['giftRecipientID'] = -1
 #print(people_df)
 for id in list(people_df.index):
     #print(id)
-    #print(people_df.loc[id, 'name'])
-    #print(people_df.loc[id, 'familyUnit'] != people_df['familyUnit'])
+    print(people_df.loc[id, 'name'])
     notFamilyBool = people_df.loc[id, 'familyUnit'] != people_df['familyUnit']
-    #print(people_df[notFamilyBool].index)
-    people_df.loc[id, 'giftRecipientID'] = random.choice(list(people_df[notFamilyBool].index))
+    #print(notFamilyBool)
+    notFamily = people_df[notFamilyBool].index
+    print(notFamily)
+    alreadyAssignedID = people_df['giftRecipientID']
+    print(alreadyAssignedID)
+    print(set(notFamily)-set(alreadyAssignedID))
+    people_df.loc[id, 'giftRecipientID'] = random.choice(list(set(notFamily)-set(alreadyAssignedID)))
 
 print(people_df)
 #while len(giftRecipienPool) !== 0:

d1530e673153ed3fae44fdebfcbfe68c7e9428e5
Jessica Sattler <jessicasattler@jessicas-air.lan>
2021-11-28 14:40:14 -0600

    Added Spanish language capabilities

--- santa.py
+++ santa.py
@@ -2,9 +2,34 @@ import pandas as pd
 import random
 import ezgmail
 from tkinter.filedialog import askopenfilename
+
+def send_email(x):
+    if x.language == 'Spanish':
+        subject = f"{x.personName}'s Regalo Secreto"
+        body = f"""Hola {x.personName},
+
+Se te ha asignado una persona muy especial para darle un regalo esta Navidad. Esa persona es {x.giftRecipientPerson}.
+
+¿Quién te va a dar un regalo a ti? Shh, eso es un secreto ...
+            
+¡Feliz Navidad!"""
+
+    else:
+        subject = f"{x.personName}'s Secret Santa Assignment"
+        body = f"""Hello {x.personName},
+                
+You have been assigned a very special person to give a gift to this Christmas. The person you have assigned is {x.giftRecipientPerson}.
+
+Who is giving you a gift? Shh, that's a secret.... 
+            
+Merry Christmas!"""
+
+    ezgmail.send(x.contact,subject,body)
+    return
+
 filenameforReading = askopenfilename()
-#filenameforReading = 'peopleSattler.csv'
 people_df = pd.read_csv(filenameforReading)
+
 while True:
     people_df['giftRecipientID'] = -1
     #print(people_df)
@@ -25,23 +50,8 @@ while True:
         continue
 
 people_df['giftRecipientPerson'] = people_df['giftRecipientID'].apply(lambda x: people_df.loc[x, 'personName'])
-
-def send_email(x):
-    subject = f"{x.personName}'s Secret Santa Assignment"
-    body = f"""Hello {x.personName},
-                
-You have been assigned a very special person to give a gift to this Christmas. The person you have assigned is {x.giftRecipientPerson}.
-
-Who is giving you a gift? Shh, that's a secret.... 
-            
-Merry Christmas!"""
-    ezgmail.send(x.contact,subject,body)
-
 people_df.apply(send_email,axis=1)
 people_df.to_csv(f"{filenameforReading[:-4]}_results.csv", index = False)
-#while len(giftRecipienPool) !== 0:
-
-
 
 
 
